3c6f132fed836eb255c505c42e4090662d30d5e9
Lubos Krnac <lubos.krnac@gmail.com>
2016-06-05 14:34:19 +0200

    Generate Spring Boot web project

---
+++ src/main/java/net/lkrnac/blog/Blog201606PageObjectPatternApplication.java
@@ -0,0 +1,12 @@
+package net.lkrnac.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog201606PageObjectPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog201606PageObjectPatternApplication.class, args);
+	}
+}
---
+++ src/test/java/net/lkrnac/blog/Blog201606PageObjectPatternApplicationTests.java
@@ -0,0 +1,18 @@
+package net.lkrnac.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Blog201606PageObjectPatternApplication.class)
+@WebAppConfiguration
+public class Blog201606PageObjectPatternApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
